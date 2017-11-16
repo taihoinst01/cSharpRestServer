f467b2016cc3e4d88969d4577d6bdcd9edaabc77
JeongHanYoung <Jeonghy@taihoinst.com>
2017-11-16 09:18:35 +0900

    update Web Config (HTTP 405 error issue)

--- restFulServer/Global.asax.cs
+++ restFulServer/Global.asax.cs
@@ -13,5 +13,18 @@ namespace restFulServer
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
         }
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            HttpContext.Current.Response.AddHeader("Access-Control-Allow-Origin", "*");
+            if (HttpContext.Current.Request.HttpMethod == "OPTIONS")
+            {
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+                HttpContext.Current.Response.AddHeader("Access-Control-Max-Age", "1728000");
+                HttpContext.Current.Response.End();
+            }
+        }
     }
+
+
 }
